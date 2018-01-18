c837aacc7aea30ccd44dcbe31981b18e38ba1cb1
Gracie <aguilargrac92@gmail.com>
2018-01-18 09:51:35 -0600

    OOP and Link List incomplete

---
+++ OOP/src/Body.java
@@ -0,0 +1,5 @@
+public class Body {
+
+    private int arms;
+    private int legs;
+}
---
+++ linked_list/main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+// for linked list java allocates 4 bytes of memory per node (int)
+// 8 bytes for double
+// for string values, 8 bytes , a string address is pointing to a string variable of unique length
+public class main{
+
+    class LinkedList{
+
+        stastic class Node {
+            int data;
+            Node next;
+        }
+        Node (int x){
+            data = x;
+            next = null;
+        }
+
+    }
+
+
+    public static void main (String [] args){
+
+
+
+
+        System.out.printf("Original Linked List: ");
+
+        PrintList(Node head);
+
+
+    }
+
+}
+
